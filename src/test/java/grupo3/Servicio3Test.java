0dd4ea97bc17cee337f7b4f9e65e6bbfe16700d7
ManuAFK28 <manustdev@gmail.com>
2025-11-15 15:04:48 -0500

    Add unit tests and clean up comments in Servicio3Test

--- src/test/java/grupo3/Servicio3Test.java
+++ src/test/java/grupo3/Servicio3Test.java
@@ -7,6 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Servicio3Test {
 
+    //Pruebas Unitarias para funcionalidad secundaria 1
+  
+
+    //Pruebas unitarias para funcionalidad secundaria 2
+
     @Test
     void testDescuentoValido(){
         double descuento = 49.0; //Dentro del rango (0 - 50)
@@ -23,25 +28,26 @@ public class Servicio3Test {
         assertFalse(resultado);
     }
 
+    //Pruebas unitarias para funcionalidad secundaria 3
+
     @Test
     void testIGVCorrecto() {
-        // Caso básico: total de 100 → 118 con IGV
+        // Caso basico: total de 100 → 118 con IGV
         assertEquals(118.0, Servicio3.calcularIGV(100.0));
     }
-    //simulación de aplicar igv con producto simulado
+    
     @Test
     void testIGVConProductoSimulado() {
-        // Creamos un producto para simular algo más real
         Producto producto = new Producto("Laptop", 200.0, 1);
-
-        // Calculamos el total del producto manualmente
-        double total = producto.getPrecio() * producto.getCantidad(); // 200 * 1 = 200
-
-        // Verificamos que el IGV se aplique correctamente
+        double total = producto.getPrecio() * producto.getCantidad();
         double totalConIGV = Servicio3.calcularIGV(total);
-
-        assertEquals(236.0, totalConIGV);  // 200 * 1.18 = 236
+        assertEquals(236.0, totalConIGV);
     }
+
+    //Pruebas unitarias para funcionalidad secundaria 4
+    
+    
+    //Pruebas unitarias para funcionalidad secundaria 5
 
     @Test
     void verificarStock_cantidadCeroTest() {
@@ -64,26 +70,5 @@ public class Servicio3Test {
         boolean resultado = Servicio3.verificarStock(productosConStockValido);
         assertTrue(resultado);
     }
-
-    @Test //Piero
-    void testIGV_SinCompra_TotalCero() {
-        double total = 0.0;  // No se compró nada
-        double totalConIGV = Servicio3.calcularIGV(total);
-
-        assertEquals(0.0, totalConIGV); 
-    }
-
-    @Test //Piero
-    void verificarStock_cantidadNegativaTest() {
-        List<Producto> productos = List.of(
-                new Producto("Mouse", 100.0, 2),
-                new Producto("Teclado", 80.0, -1)  // cantidad negativa → inválida
-        );
-
-        boolean resultado = Servicio3.verificarStock(productos);
-
-        assertFalse(resultado);
-    }
-
-
 }
+
