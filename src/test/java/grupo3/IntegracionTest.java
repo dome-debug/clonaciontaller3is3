b1f8d3603f77780d171bd30a99ed7b0e6cbe5392
RodrigoTaboada <118780496+RodrigoTaboadaC@users.noreply.github.com>
2025-11-15 02:15:36 -0500

    Update IntegracionTest.java

--- src/test/java/grupo3/IntegracionTest.java
+++ src/test/java/grupo3/IntegracionTest.java
@@ -22,5 +22,17 @@ public class IntegracionTest {
         assertEquals(2867.4, totalIGV, 0.1);
     }
 
+    @Test
+    void ErrorFuncionSecundariaTest() {
+        List<Producto> productos = List.of(
+                new Producto("Laptop", 2500, 1),
+                new Producto("Mouse", 100, 0)
+        );
+        double total = Pedido.calcularTotalPedido(productos, 0);
+        assertTrue(total > 0);//Verificamos que la función base produzca un total positivo
+
+        boolean resultadoStock = ServicioX.verificarStock(productos);
+        assertFalse(resultadoStock);
+    }
 
 }
